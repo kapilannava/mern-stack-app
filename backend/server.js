eea0f25649f313faee9527f9844f645a4359a5ea
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-02-24 05:19:09 -0500

    Changed express routing

--- backend/server.js
+++ backend/server.js
@@ -72,10 +72,10 @@ todoRoutes.route('/update/:id').post(function(req, res){
 app.use('/todos', todoRoutes);
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static ( 'client/build' ));
+    app.use(express.static ( 'mern-todo-app/build' ));
 
-    app.get('/*', (req, res) =>{
-        res.sendFile(path.join(__dirname, './../mern-todo-app/public/', 'index.html')); // relative path
+    app.get('*', (req, res) =>{
+        res.sendFile(path.join(__dirname, 'mern-todo-app', 'build', 'index.html')); // relative path
     });
 }
 
