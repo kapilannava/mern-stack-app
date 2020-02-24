bb65d91a9e2f645eb1e50991accee4549ded8164
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-02-24 04:43:41 -0500

    re-configured index path to absolute path

--- backend/server.js
+++ backend/server.js
@@ -75,7 +75,7 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static ( 'client/build' ));
 
     app.get('/*', (req, res) =>{
-        res.sendFile(path.join(__dirname, '../mern-todo-app/public/index.html')); // relative path
+        res.sendFile(path.join(__dirname, '../mern-todo-app/public', 'index.html')); // relative path
     });
 }
 
