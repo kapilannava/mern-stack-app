57dd8fdb47e05f8b0e82081bd4b21b821bbf11ff
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-02-24 04:26:44 -0500

    Updated GET request in server.js

--- backend/server.js
+++ backend/server.js
@@ -74,7 +74,7 @@ app.use('/todos', todoRoutes);
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static ( 'client/build' ));
 
-    app.get('*', (req, res) =>{
+    app.get('/*', (req, res) =>{
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
     });
 }
